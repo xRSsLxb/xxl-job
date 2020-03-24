16dac2aa8a948c3bffbb5f313de84224a6d6a657
liuxb <liuxb@yibanquan.com.cn>
2020-03-24 17:36:17 +0800

    集成

---
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/JobHandlerDemo.java
@@ -0,0 +1,31 @@
+package com.xxl.job.executor.service.jobhandler;
+/**
+ * Created by bxt on 2020/3/24.
+ */
+
+
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import com.xxl.job.core.log.XxlJobLogger;
+import org.springframework.stereotype.Component;
+
+/**
+ * XxlJob开发示例（Bean模式）.
+ *
+ * @ClassName JobHandlerDemo
+ * @Author liuxb
+ * @Date 2020/3/24
+ * @Version 1.0
+ */
+
+@Component
+public class JobHandlerDemo {
+    
+    
+    @XxlJob("demoJobHandlerExecute")
+    public ReturnT<String> execute(String param) throws Exception {
+        XxlJobLogger.log("调用成功...");
+        System.out.println("Hello World...");
+        return ReturnT.SUCCESS;
+    }
+}
